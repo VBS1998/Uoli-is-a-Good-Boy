172ca8545a4b9186f59243f557722c1295af940f
VBS1998 <gustavo.vb@hotmail.com>
2018-11-19 17:09:26 -0200

    comecando ronda

---
+++ src/ronda.c
@@ -0,0 +1,26 @@
+#include "api_robot.h"
+
+int _start(){
+
+    motor_cfg_t *motor = malloc(sizeof(motor_cfg_t));
+
+    while(1){
+        for(int t = 1; t <= 50; t=t+2){
+            set_time(0);
+
+            //vai para frente:
+            motor->id = 0;
+            motor->speed = 30;
+            set_motor_speed(motor);
+            motor->id = 1;
+            motor->speed = 30;
+            set_motor_speed(motor);
+
+            while(get_time() < t*1000){
+            }
+
+        }
+
+    }
+    free(motor);
+}
